1bcdc65eb759d149a81a81e082f00849474aa122
Fernando Simbolon <fernando.simbolon@grabtaxi.com>
2020-08-09 16:11:33 +0700

    add tc for edit gist

--- github/pages/gists/detail_gist.js
+++ github/pages/gists/detail_gist.js
@@ -3,6 +3,9 @@ module.exports = {
   ],
   elements: {
     edit_button: 'a.btn.btn-sm svg.octicon.octicon-pencil',
-    delete_button: 'button.btn-danger'
+    delete_button: 'button.btn-danger',
+    file_name: 'a.css-truncate',
+    file_description: 'div[itemprop="about"]',
+    file_content: 'div table tbody'
   },
 };
--- github/pages/gists/list_gists.js
+++ github/pages/gists/list_gists.js
@@ -3,6 +3,7 @@ const util = require('util');
 module.exports = {
   commands: [{
       load_gist_list: navigate_to_user_gist,
+      select_gist: select_gist
     }],
   elements: {
     gist_link: 'div[class="gist-snippet"] div div div span a:nth-child(2)',
@@ -14,4 +15,12 @@ function navigate_to_user_gist(user_id){
   this.navigate('https://gist.github.com/' + user_id)
     .waitForElementVisible('@gist_container');
   return this;
+}
+
+//select any random gist
+//TODO : set random
+function select_gist(){
+  this.waitForElementVisible('body')
+    .click('@gist_link')
+    .waitForElementVisible('a.css-truncate')
 }
